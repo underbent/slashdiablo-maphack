underbent/slashdiablo-maphack

commit 100f25136d600ff82a664b341e8d4373f859f45d

Fix noticeable delays when entering new level
There are two fixes here. The original code was calling CreateMap three
times on three different collision maps for the same level, which is
wasteful. Even reducing this to one call still gives a visible pause
when entering a new level, so also relegate CreateMap in a background
thread.

BH/Modules/AutoTele/AutoTele.cpp
@@ -56,6 +56,9 @@ void AutoTele::OnLoad() {
 }
 
 void AutoTele::OnAutomapDraw() {
+	if (WaitingForMapData()) {
+		return;
+	}
 
 	POINT MyPos, Pos;
 	Drawing::Hook::ScreenToAutomap(&MyPos, D2CLIENT_GetUnitX(D2CLIENT_GetPlayerUnit()), D2CLIENT_GetUnitY(D2CLIENT_GetPlayerUnit()));
@@ -86,12 +89,17 @@ void AutoTele::OnAutomapDraw() {
 }
 
 void AutoTele::OnLoop() {
+	if (WaitingForMapData()) {
+		return;
+	}
 
-	if(GetPlayerArea() && LastArea != GetPlayerArea() && D2CLIENT_GetPlayerUnit()) {
-		LastArea = GetPlayerArea();
+	DWORD playerArea = GetPlayerArea();
+	if(playerArea && LastArea != playerArea && D2CLIENT_GetPlayerUnit()) {
+		LastArea = playerArea;
 		if(LastArea != MAP_A4_THE_CHAOS_SANCTUARY)
 			CSID = 0;
-		GetVectors();
+		// Loading the new area can cause noticeable hangs, so run in a background thread
+		LoadHandle = CreateThread(0, 0, LoadNewArea, this, 0, 0);
 	}
 
 	if(LastArea == MAP_A4_THE_CHAOS_SANCTUARY)
@@ -191,7 +199,8 @@ void AutoTele::OnKey(bool up, BYTE key, LPARAM lParam, bool* block) {
 	} 
 	else if (key == WPKey) 
 	{
-	} else if (key == PrevKey) 
+	}
+	else if (key == PrevKey) 
 	{
 	}
 }
@@ -220,9 +229,31 @@ void AutoTele::OnGamePacketRecv(BYTE* packet, bool* block) {
 }
 
 void AutoTele::GetVectors() {
+	DWORD MyArea = GetPlayerArea();
+	DWORD Areas[2] = {MyArea, 0};
+	DWORD AreaCount = 1;
+	BOOL buildCollisionMap = false;
+
+	// Usually all markers/paths are in the current area (exceptions include River of Flame,
+	// which has its destination marker in the Chaos Sanctuary). First scan the vectors to see
+	// which areas we need to load.
+	for (int i = 0; i < 4; i++) {
+		Vector V = vVector[MyArea*4+i];
+		if (V.dwType == EXIT || (MyArea == MAP_A2_CANYON_OF_THE_MAGI && V.dwType == 0 && V.Id == 0)) {
+			if (V.Area) {
+				Areas[1] = V.Area;
+				AreaCount = 2;
+			}
+			buildCollisionMap = true;
+		}
+	}
+
+	CCollisionMap g_collisionMap;
+	if (buildCollisionMap) {
+		buildCollisionMap = g_collisionMap.CreateMap(Areas, AreaCount);  //create a cmap for the current area
+	}
 
 	for(int i = 0;i<4;i++) {
-		DWORD MyArea = GetPlayerArea();
 		Vectors[i].x = 0;
 		Vectors[i].y = 0;
 		Vector V = vVector[MyArea*4+i];
@@ -246,23 +277,13 @@ void AutoTele::GetVectors() {
 			if(V.Area)
 				Vectors[i] = FindPresetLocation(V.dwType, V.Id, V.Area);
 			else
-				Vectors[i] = FindPresetLocation(V.dwType, V.Id, GetPlayerArea());
+				Vectors[i] = FindPresetLocation(V.dwType, V.Id, MyArea);
 		}
 
 		if(V.dwType == EXIT) {
-			CCollisionMap g_collisionMap; //init the collisionmap
-
-			DWORD Areas[2];
-			DWORD AreaCount = 1;
-			Areas[0] = GetPlayerArea();
-
-			if(V.Area) {
-				Areas[1] = V.Area;
-				AreaCount = 2;
-			}
-
-			if(!g_collisionMap.CreateMap(Areas, AreaCount))//create a cmap for the current area
+			if (!buildCollisionMap) {
 				continue;
+			}
 
 			LPLevelExit ExitArray[0x40];//declare an array of exits to store the exits in later
 
@@ -649,5 +670,23 @@ DWORD AutoTele::GetUnitByXY(DWORD X, DWORD Y, Room2* pRoom) {
 					return pUnit->dwUnitId;
 		pUnit=pUnit->pListNext;
 	}
+	return 0;
+}
+
+bool AutoTele::WaitingForMapData() {
+	if (LoadHandle) {
+		DWORD wait = WaitForSingleObject(LoadHandle, 0);
+		if (wait == WAIT_TIMEOUT) {
+			return true;
+		}
+		CloseHandle(LoadHandle);
+		LoadHandle = NULL;
+	}
+	return false;
+}
+
+DWORD WINAPI LoadNewArea(VOID* lpvoid) {
+	AutoTele *at = (AutoTele*)lpvoid;
+	at->GetVectors();
 	return 0;
 }

BH/Modules/AutoTele/AutoTele.h
@@ -30,6 +30,7 @@ class AutoTele : public Module {
 		DWORD LastArea;
 		POINT Vectors[4];
 		CArrayEx <POINT, POINT> TPath;
+		HANDLE LoadHandle;
 
 		//functions
 		DWORD GetPlayerArea();
@@ -42,17 +43,18 @@ class AutoTele : public Module {
 		bool CastOnMap(WORD x, WORD y, bool Left);
 		bool Interact(DWORD UnitId, DWORD UnitType);
 		DWORD GetUnitByXY(DWORD X, DWORD Y, Room2* pRoom);
-		void GetVectors();
-				
+		bool WaitingForMapData();
+
 	public:
-		AutoTele() : Module("AutoTele") {};
+		AutoTele() : Module("AutoTele"), LoadHandle(NULL) {};
 		void OnLoad();
 		void OnLoop();
 		void OnAutomapDraw();
 		void OnKey(bool up, BYTE key, LPARAM lParam, bool* block);
 		void OnGamePacketRecv(BYTE* packet, bool* block);
-		
+
 		std::map<string, Toggle>* GetToggles() { return &Toggles; }
+		void GetVectors();
 
 		static Level* GetLevel(Act* pAct, int level);
 		static DWORD GetDistanceSquared(DWORD x1, DWORD y1, DWORD x2, DWORD y2);
@@ -64,3 +66,5 @@ enum TeleType {
 	WP,
 	Prev
 };
+
+DWORD WINAPI LoadNewArea(VOID* lpvoid);

commit 499f410c52b07c9ba4285cacc0a1efa75d710145

Roll back the background thread part of 100f2513
Commit 100f2513 introduced sporadic crashes that occasionally happened
when moving to a new area then immediately back to the previous area
before the background thread finished. The problem is that GetVectors()
depends on game state (act, player position/path/room, etc) that can't
be allowed to change underneath the background thread. We can cache most
of it while the thread runs. But certain stuff in the player's path (the
room mainly) gets passed to Add/RemoveRoomData, and I'm not sure if it
is stable while moving between areas/acts. Seems like playing with fire,
so punt on this for now. We still keep the factor-of-3 speedup from the
previous commit, at least.

BH/Modules/AutoTele/AutoTele.cpp
@@ -56,9 +56,9 @@ void AutoTele::OnLoad() {
 }
 
 void AutoTele::OnAutomapDraw() {
-	if (WaitingForMapData()) {
-		return;
-	}
+	//if (WaitingForMapData()) {
+	//	return;
+	//}
 
 	POINT MyPos, Pos;
 	Drawing::Hook::ScreenToAutomap(&MyPos, D2CLIENT_GetUnitX(D2CLIENT_GetPlayerUnit()), D2CLIENT_GetUnitY(D2CLIENT_GetPlayerUnit()));
@@ -89,17 +89,18 @@ void AutoTele::OnAutomapDraw() {
 }
 
 void AutoTele::OnLoop() {
-	if (WaitingForMapData()) {
-		return;
-	}
+	//if (WaitingForMapData()) {
+	//	return;
+	//}
 
 	DWORD playerArea = GetPlayerArea();
 	if(playerArea && LastArea != playerArea && D2CLIENT_GetPlayerUnit()) {
 		LastArea = playerArea;
 		if(LastArea != MAP_A4_THE_CHAOS_SANCTUARY)
 			CSID = 0;
-		// Loading the new area can cause noticeable hangs, so run in a background thread
-		LoadHandle = CreateThread(0, 0, LoadNewArea, this, 0, 0);
+		GetVectors();
+		//LoadHandle = CreateThread(0, 0, LoadNewArea, this, 0, 0);
+		//return;
 	}
 
 	if(LastArea == MAP_A4_THE_CHAOS_SANCTUARY)
@@ -298,6 +299,7 @@ void AutoTele::GetVectors() {
 					continue;
 				}
 			}
+			// fixme: should free the ExitArray entries after
 		}
 	}
 }

BH/Modules/ItemMover/ItemMover.cpp
@@ -159,22 +159,22 @@ bool ItemMover::FindDestination(int xpac, int destination, unsigned int itemId, 
 
 void ItemMover::PickUpItem() {
 	BYTE PacketData[5] = {0x19,0,0,0,0};
-    *reinterpret_cast<int*>(PacketData + 1) = ActivePacket.itemId;
+	*reinterpret_cast<int*>(PacketData + 1) = ActivePacket.itemId;
 	D2NET_SendPacket(5, 1, PacketData);
 }
 
 void ItemMover::PutItemInContainer() {
 	BYTE PacketData[17] = {0x18,0,0,0,0};
-    *reinterpret_cast<int*>(PacketData + 1) = ActivePacket.itemId;
-    *reinterpret_cast<int*>(PacketData + 5) = ActivePacket.x;
-    *reinterpret_cast<int*>(PacketData + 9) = ActivePacket.y;
-    *reinterpret_cast<int*>(PacketData + 13)= ActivePacket.destination;
+	*reinterpret_cast<int*>(PacketData + 1) = ActivePacket.itemId;
+	*reinterpret_cast<int*>(PacketData + 5) = ActivePacket.x;
+	*reinterpret_cast<int*>(PacketData + 9) = ActivePacket.y;
+	*reinterpret_cast<int*>(PacketData + 13)= ActivePacket.destination;
 	D2NET_SendPacket(17, 1, PacketData);
 }
 
 void ItemMover::PutItemOnGround() {
 	BYTE PacketData[5] = {0x17,0,0,0,0};
-    *reinterpret_cast<int*>(PacketData + 1) = ActivePacket.itemId;
+	*reinterpret_cast<int*>(PacketData + 1) = ActivePacket.itemId;
 	D2NET_SendPacket(5, 1, PacketData);
 }
 