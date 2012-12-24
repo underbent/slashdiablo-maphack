underbent/slashdiablo-maphack

commit 607731c73a9bbc002821f318a92e7c3b72d98576

Add auto-looting mode for hardcore

BH/Modules/Party/Party.cpp
@@ -7,13 +7,18 @@
 using namespace std;
 using namespace Drawing;
 Drawing::Hook* PartyHook;
+Drawing::Hook* LootHook;
 
 void Party::OnLoad() {
 	Toggles["Enabled"] = BH::config->ReadToggle("Party Enabled", "None", true);
+	Toggles["LootEnabled"] = BH::config->ReadToggle("Looting Enabled", "None", true);
+	doCheckParty = Toggles["Enabled"].state || Toggles["LootEnabled"].state;
 	c = 0;
 
 	PartyHook = new Drawing::Checkhook(Drawing::InGame, 100, 100, &Toggles["Enabled"].state, "Autoparty Enabled");
 	PartyHook->SetActive(0);
+	LootHook = new Drawing::Checkhook(Drawing::InGame, 240, 100, &Toggles["LootEnabled"].state, "Autoloot Enabled");
+	LootHook->SetActive(0);
 }
 
 void Party::OnUnload() {
@@ -21,18 +26,29 @@ void Party::OnUnload() {
 }
 
 void Party::OnLoop() {
-	if(Toggles["Enabled"].state)
+	if(doCheckParty)
 		CheckParty();
 	if(D2CLIENT_GetUIState(0x16) && PartyHook->IsActive() == 0)
 		PartyHook->SetActive(1);
 	else if(D2CLIENT_GetUIState(0x16) == 0 && PartyHook->IsActive())
 		PartyHook->SetActive(0);
+
+	BnetData* pData = (*p_D2LAUNCH_BnData);
+	if(!pData)
+		return;
+	if(D2CLIENT_GetUIState(0x16) && LootHook->IsActive() == 0 && pData->nCharFlags & PLAYER_TYPE_HARDCORE)
+		LootHook->SetActive(1);
+	else if(D2CLIENT_GetUIState(0x16) == 0 && LootHook->IsActive())
+		LootHook->SetActive(0);
 }
 
 void Party::CheckParty() {
 	if(c == 0) {
+		bool valid = true;
+		std::map<std::string, bool> CurrentParty;
 		UnitAny* Me = *p_D2CLIENT_PlayerUnit;
 		RosterUnit* MyRoster = FindPlayerRoster(Me->dwUnitId);
+		BnetData* pData = (*p_D2LAUNCH_BnData);
 
 		for(RosterUnit* Party = (*p_D2CLIENT_PlayerUnitList);Party;Party = Party->pNext) {
 			if(!_stricmp(Party->szName, MyRoster->szName))
@@ -45,9 +61,22 @@ void Party::CheckParty() {
 				(Party->wPartyId != INVALID_PARTY_ID && Party->dwPartyFlags & PARTY_NOT_IN_PARTY)) {
 				// Avoid crashing when multiple players in a game have auto-party enabled
 				// (there seems to be a brief window in which the party data can be invalid)
+				valid = false;
 				continue;
 			}
-			if(Party->wPartyId == INVALID_PARTY_ID || Party->wPartyId != MyRoster->wPartyId) {
+			if (pData && pData->nCharFlags & PLAYER_TYPE_HARDCORE) {
+				CurrentParty[Party->szName] = true;
+				if (Toggles["LootEnabled"].state) {
+					string s(Party->szName);
+					if (LootingPermission.find(s) == LootingPermission.end()) {
+						BYTE PacketData[7] = {0x5d,1,1,0,0,0,0};
+						*reinterpret_cast<int*>(PacketData + 3) = Party->dwUnitId;
+						D2NET_SendPacket(7, 1, PacketData);
+						LootingPermission[s] = true;
+					}
+				}
+			}
+			if ((Party->wPartyId == INVALID_PARTY_ID || Party->wPartyId != MyRoster->wPartyId) && Toggles["Enabled"].state) {
 				if(Party->dwPartyFlags & PARTY_INVITED_YOU) {
 					D2CLIENT_ClickParty(Party, 2);
 					c++;
@@ -59,6 +88,16 @@ void Party::CheckParty() {
 					D2CLIENT_ClickParty(Party, 2);
 					c++;
 					return;
+				}
+			}
+		}
+
+		if (valid) {
+			for (auto it = LootingPermission.cbegin(); it != LootingPermission.cend(); ) {
+				if (CurrentParty.find((*it).first) == CurrentParty.end()) {
+					LootingPermission.erase(it++);
+				} else {
+					++it;
 				}
 			}
 		}
@@ -81,4 +120,8 @@ void Party::OnKey(bool up, BYTE key, LPARAM lParam, bool* block)  {
 			return;
 		}
 	}
+}
+
+void Party::OnGameExit() {
+	LootingPermission.clear();
 }

BH/Modules/Party/Party.h
@@ -11,13 +11,15 @@
 class Party : public Module {
 	private:
 		map<std::string, Toggle> Toggles;
+		map<std::string, bool> LootingPermission;
 		void CheckParty();
 		int c;
-
+		bool doCheckParty;
 	public:
-		Party() : Module("Party") {};
+		Party() : Module("Party"), doCheckParty(false) {};
 		void OnLoad();
 		void OnUnload();
 		void OnLoop();
 		void OnKey(bool up, BYTE key, LPARAM lParam, bool* block);
+		void OnGameExit();
 };

Packaging/BH.cfg
@@ -72,6 +72,7 @@ Prev Color:		0x5B
 
 //Party Configuration
 Party Enabled:		False, VK_9
+Looting Enabled:	False, VK_7
 
 //Spam Filter Configuration
 Log Chat:		False, None

commit 67adf33e4c9184b7074e16fd1066cfef0f50b4d6

Fix auto-party/loot bug introduced in 607731c7

BH/Modules/Party/Party.cpp
@@ -12,7 +12,6 @@ Drawing::Hook* LootHook;
 void Party::OnLoad() {
 	Toggles["Enabled"] = BH::config->ReadToggle("Party Enabled", "None", true);
 	Toggles["LootEnabled"] = BH::config->ReadToggle("Looting Enabled", "None", true);
-	doCheckParty = Toggles["Enabled"].state || Toggles["LootEnabled"].state;
 	c = 0;
 
 	PartyHook = new Drawing::Checkhook(Drawing::InGame, 100, 100, &Toggles["Enabled"].state, "Autoparty Enabled");
@@ -26,7 +25,7 @@ void Party::OnUnload() {
 }
 
 void Party::OnLoop() {
-	if(doCheckParty)
+	if(Toggles["Enabled"].state || Toggles["LootEnabled"].state)
 		CheckParty();
 	if(D2CLIENT_GetUIState(0x16) && PartyHook->IsActive() == 0)
 		PartyHook->SetActive(1);

BH/Modules/Party/Party.h
@@ -14,9 +14,8 @@ class Party : public Module {
 		map<std::string, bool> LootingPermission;
 		void CheckParty();
 		int c;
-		bool doCheckParty;
 	public:
-		Party() : Module("Party"), doCheckParty(false) {};
+		Party() : Module("Party") {};
 		void OnLoad();
 		void OnUnload();
 		void OnLoop();