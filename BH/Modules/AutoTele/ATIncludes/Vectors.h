88dd16fcb5f085a9c86012fdd4c785d3425694d7
underbent <underbent@gmail.com>
2013-12-10 22:07:44 -0500

    Add some missing map lines

--- BH/Modules/AutoTele/ATIncludes/Vectors.h
+++ BH/Modules/AutoTele/ATIncludes/Vectors.h
@@ -40,7 +40,7 @@ Vector vVector[] = {
 	{EXIT, MAP_A1_BLOOD_MOOR},								
 
 	{EXIT, MAP_A1_CAVE_LEVEL_2},							//0x9
-	{UNIT_MONSTER, 736},
+	{UNIT_MONSTER, 736},									//coldcrow
 	{0},
 	{EXIT, MAP_A1_COLD_PLAINS},
 
@@ -80,9 +80,20 @@ Vector vVector[] = {
 	{0},
 	{EXIT, MAP_A1_PIT_LEVEL_1},
 
-	{0}, {0}, {0}, {0},										//0x11
-	{0}, {0}, {0}, {0},										//0x12
-	{0}, {0}, {0}, {0},										//0x13
+	{UNIT_MONSTER, 805},									//0x11
+	{UNIT_MONSTER, 805},									//blood raven
+	{0},
+	{EXIT, MAP_A1_COLD_PLAINS},
+
+	{UNIT_OBJECT, 397},										//0x12
+	{UNIT_OBJECT, 397},
+	{0},
+	{EXIT, MAP_A1_BURIAL_GROUNDS},
+
+	{UNIT_OBJECT, 397},										//0x13
+	{UNIT_OBJECT, 397},
+	{0},
+	{EXIT, MAP_A1_BURIAL_GROUNDS},
 
 	{EXIT, MAP_A1_TOWER_CELLAR_LEVEL_1},					//0x14
 	{EXIT, MAP_A1_TOWER_CELLAR_LEVEL_1},
@@ -114,7 +125,7 @@ Vector vVector[] = {
 
 
 	{UNIT_MONSTER, 740},									//0x19
-	{UNIT_MONSTER, 740},
+	{UNIT_MONSTER, 740},									//countess
 	{0},
 	{EXIT, MAP_A1_TOWER_CELLAR_LEVEL_4},
 
@@ -124,7 +135,7 @@ Vector vVector[] = {
 	{XY, 15141, 5091, MAP_A1_TAMOE_HIGHLAND},
 
 
-	{EXIT, MAP_A1_BARRACKS},						//0x1b
+	{EXIT, MAP_A1_BARRACKS},								//0x1b
 	{EXIT, MAP_A1_BARRACKS},
 	{UNIT_OBJECT, 119},
 	{EXIT, MAP_A1_MONASTERY_GATE},
@@ -246,7 +257,7 @@ Vector vVector[] = {
 	{EXIT, MAP_A2_SEWERS_LEVEL_2},
 
 	{UNIT_TILE, 29},										//0x32
-	{UNIT_TILE, 29},
+	{UNIT_TILE, 29},										//harem lvl 2 stairs; use UNIT_TILE rather than EXIT because there are two?
 	{0},
 	{EXIT, MAP_A2_LUT_GHOLEIN},
 
@@ -270,7 +281,10 @@ Vector vVector[] = {
 	{0},
 	{EXIT, MAP_A2_PALACE_CELLAR_LEVEL_2},
 
-	{0}, {0}, {0}, {0},										//0x37 -> stony tomb
+	{EXIT, MAP_A2_STONY_TOMB_LEVEL_2},						//0x37 -> stony tomb
+	{EXIT, MAP_A2_STONY_TOMB_LEVEL_2},
+	{0},
+	{EXIT, MAP_A2_ROCKY_WASTE},
 
 	{EXIT, MAP_A2_HALLS_OF_THE_DEAD_LEVEL_2},				//0x38
 	{EXIT, MAP_A2_HALLS_OF_THE_DEAD_LEVEL_2},
@@ -287,7 +301,10 @@ Vector vVector[] = {
 	{0},
 	{EXIT, MAP_A2_VALLEY_OF_SNAKES},
 
-	{0}, {0}, {0}, {0},										//0x3b -> stony tomb lvl 2
+	{UNIT_OBJECT, 397},										//0x3b -> stony tomb lvl 2
+	{UNIT_OBJECT, 397},
+	{0},
+	{EXIT, MAP_A2_STONY_TOMB_LEVEL_1},
 
 	{UNIT_OBJECT, 354},										//0x3c
 	{UNIT_OBJECT, 354},
@@ -314,7 +331,10 @@ Vector vVector[] = {
 	{0},
 	{EXIT, MAP_A2_MAGGOT_LAIR_LEVEL_2},
 
-	{0}, {0}, {0}, {0},										//0x41 ancient tunnels
+	{UNIT_OBJECT, 397},										//0x41 ancient tunnels
+	{UNIT_OBJECT, 397},
+	{0},
+	{EXIT, MAP_A2_LOST_CITY},
 
 	{UNIT_OBJECT, 152},										//0x42
 	{UNIT_OBJECT, 152},
@@ -418,17 +438,61 @@ Vector vVector[] = {
 	{UNIT_OBJECT, 237},
 	{EXIT, MAP_A3_KURAST_CAUSEWAY},
 
-	{0}, {0}, {0}, {0},										//0x54 -> arachnid lair
-	{0}, {0}, {0}, {0},										//0x55 -> spider cavern
-	{0}, {0}, {0}, {0},										//0x56 -> swampy pit lvl 1
-	{0}, {0}, {0}, {0},										//0x57 -> swampy pit lvl 2
-	{0}, {0}, {0}, {0},										//0x58 -> flayer dungeon lvl 1
-	{0}, {0}, {0}, {0},										//0x59 -> flayer dungeon lvl 2
-	{0}, {0}, {0}, {0},										//0x5a -> swampy pit lvl 3
-	{0}, {0}, {0}, {0},										//0x5b -> flayer dungeon lvl 3
-	{0}, {0}, {0}, {0},										//0x5c -> sewer lvl 1
-	{0}, {0}, {0}, {0},										//0x5d -> sewer lvl 2
-	{0}, {0}, {0}, {0},										//0x5e -> ruined temple
+	{UNIT_OBJECT, 397},										//0x54 -> arachnid lair
+	{UNIT_OBJECT, 397},
+	{0},
+	{EXIT, MAP_A3_SPIDER_FOREST},
+
+	{UNIT_OBJECT, 407},										//0x55 -> spider cavern
+	{UNIT_OBJECT, 407},
+	{0},
+	{EXIT, MAP_A3_SPIDER_FOREST},
+
+	{EXIT, MAP_A3_SWAMPY_PIT_LEVEL_2},						//0x56 -> swampy pit lvl 1
+	{EXIT, MAP_A3_SWAMPY_PIT_LEVEL_2},
+	{0},
+	{EXIT, MAP_A3_FLAYER_JUNGLE},
+
+	{EXIT, MAP_A3_SWAMPY_PIT_LEVEL_3},						//0x57 -> swampy pit lvl 2
+	{EXIT, MAP_A3_SWAMPY_PIT_LEVEL_3},
+	{0},
+	{EXIT, MAP_A3_SWAMPY_PIT_LEVEL_1},
+
+	{EXIT, MAP_A3_FLAYER_DUNGEON_LEVEL_2},					//0x58 -> flayer dungeon lvl 1
+	{EXIT, MAP_A3_FLAYER_DUNGEON_LEVEL_2},
+	{0},
+	{EXIT, MAP_A3_FLAYER_JUNGLE},
+
+	{EXIT, MAP_A3_FLAYER_DUNGEON_LEVEL_3},					//0x59 -> flayer dungeon lvl 2
+	{EXIT, MAP_A3_FLAYER_DUNGEON_LEVEL_3},
+	{0},
+	{EXIT, MAP_A3_FLAYER_DUNGEON_LEVEL_1},
+
+	{UNIT_OBJECT, 397},										//0x5a -> swampy pit lvl 3
+	{UNIT_OBJECT, 397},
+	{0},
+	{EXIT, MAP_A3_SWAMPY_PIT_LEVEL_2},
+
+	{UNIT_OBJECT, 406},										//0x5b -> flayer dungeon lvl 3
+	{UNIT_OBJECT, 406},
+	{0},
+	{EXIT, MAP_A3_FLAYER_DUNGEON_LEVEL_2},
+
+	{EXIT, MAP_A3_SEWERS_LEVEL_2},							//0x5c -> sewer lvl 1
+	{EXIT, MAP_A3_SEWERS_LEVEL_2},
+	{0},
+	{0},
+
+	{UNIT_OBJECT, 405},										//0x5d -> sewer lvl 2
+	{UNIT_OBJECT, 405},
+	{0},
+	{EXIT, MAP_A3_SEWERS_LEVEL_1},
+
+	{UNIT_OBJECT, 193},										//0x5e -> ruined temple
+	{UNIT_OBJECT, 193},
+	{0},
+	{EXIT, MAP_A3_KURAST_BAZAAR},
+
 	{0}, {0}, {0}, {0},										//0x5f -> disused fane
 	{0}, {0}, {0}, {0},										//0x60 -> forgotten reliquary
 	{0}, {0}, {0}, {0},										//0x61 -> forgotton temple
@@ -465,7 +529,7 @@ Vector vVector[] = {
 
 
 	{EXIT, MAP_A4_CITY_OF_THE_DAMNED},						//0x69
-	{UNIT_MONSTER, 256},
+	{UNIT_MONSTER, 256},									//izual
 	{UNIT_OBJECT, 238},
 	{EXIT, MAP_A4_OUTER_STEPPES},
 
@@ -482,7 +546,7 @@ Vector vVector[] = {
 	{EXIT, MAP_A4_CITY_OF_THE_DAMNED},
 
 
-	{UNIT_OBJECT, 392, 1337},										//0x6c
+	{UNIT_OBJECT, 392, 1337},								//0x6c
 	{UNIT_OBJECT, 255},
 	{0},
 	{EXIT, MAP_A4_RIVER_OF_FLAME},
@@ -495,7 +559,7 @@ Vector vVector[] = {
 	{0}, {0}, {0}, {0},										//0x6d
 
 	{EXIT, MAP_A5_FRIGID_HIGHLANDS},						//0x6e
-	{UNIT_MONSTER, 776},
+	{UNIT_MONSTER, 776},									//shenk
 	{0},
 	{EXIT, MAP_A5_HARROGATH},
 
@@ -524,19 +588,25 @@ Vector vVector[] = {
 	{UNIT_OBJECT, 511},
 	{EXIT, MAP_A5_CRYSTALLINE_PASSAGE},
 
-	{0}, {0}, {0}, {0},										//0x74 -> drifter cavern
+	{UNIT_OBJECT, 397},										//0x74 -> drifter cavern
+	{UNIT_OBJECT, 397},
+	{0},
+	{EXIT, MAP_A5_GLACIAL_TRAIL},
 
 	{EXIT, MAP_A5_THE_ANCIENTS_WAY},						//0x75
 	{EXIT, MAP_A5_THE_ANCIENTS_WAY},
 	{UNIT_OBJECT, 496},
-	{EXIT, MAP_A5_ARREAT_PLATEAU},
+	{EXIT, MAP_A5_GLACIAL_TRAIL},
 
 	{EXIT, MAP_A5_ARREAT_SUMMIT},							//0x76
 	{EXIT, MAP_A5_ARREAT_SUMMIT},
 	{UNIT_OBJECT, 511},
 	{EXIT, MAP_A5_FROZEN_TUNDRA},
 
-	{0}, {0}, {0}, {0},										//0x77 -> icy cellar
+	{UNIT_OBJECT, 397},										//0x77 -> icy cellar
+	{UNIT_OBJECT, 397},
+	{0},
+	{EXIT, MAP_A5_THE_ANCIENTS_WAY},
 
 	{EXIT, MAP_A5_WORLDSTONE_KEEP_LEVEL_1},					//0x78
 	{EXIT, MAP_A5_WORLDSTONE_KEEP_LEVEL_1},
@@ -563,9 +633,20 @@ Vector vVector[] = {
 	{0},
 	{EXIT, MAP_A5_HALLS_OF_PAIN},
 
-	{0}, {0}, {0}, {0},										//0x7d -> abaddon
-	{0}, {0}, {0}, {0},										//0x7e -> pit of acheron
-	{0}, {0}, {0}, {0},										//0x7f -> infernal pit
+	{UNIT_OBJECT, 397},										//0x7d -> abaddon
+	{UNIT_OBJECT, 397},
+	{0},
+	{EXIT, MAP_A5_FRIGID_HIGHLANDS},
+
+	{UNIT_OBJECT, 397},										//0x7e -> pit of acheron
+	{UNIT_OBJECT, 397},
+	{0},
+	{EXIT, MAP_A5_ARREAT_PLATEAU},
+
+	{UNIT_OBJECT, 397},										//0x7f -> infernal pit
+	{UNIT_OBJECT, 397},
+	{0},
+	{EXIT, MAP_A5_FROZEN_TUNDRA},
 
 	{EXIT, MAP_A5_WORLDSTONE_KEEP_LEVEL_2},					//0x80
 	{EXIT, MAP_A5_WORLDSTONE_KEEP_LEVEL_2},
@@ -589,15 +670,18 @@ Vector vVector[] = {
 
 	{0}, {0}, {0}, {0},										//0x84 -> worldstone keep
 
-	{UNIT_OBJECT, 397},										//0x85
-	{UNIT_OBJECT, 397},
+	{UNIT_MONSTER, 707},									//0x85
+	{UNIT_MONSTER, 707},									//lilith
 	{0},
 	{0},
 
-	{0}, {0}, {0}, {0},										//0x86 -> forgotton sands
+	{UNIT_MONSTER, 708},									//0x86 -> forgotton sands
+	{UNIT_MONSTER, 708},									//uber duriel
+	{0},
+	{0},
 
-	{UNIT_OBJECT, 397},										//0x87
-	{UNIT_OBJECT, 397},
+	{UNIT_MONSTER, 706},									//0x87
+	{UNIT_MONSTER, 706},									//uber izual
 	{0},
 	{0}
 };
