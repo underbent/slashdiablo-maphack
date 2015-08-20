5f3897b65c44e2050009821445dcdec37540a850
ckellner <kellnerc13@gmail.com>
2015-08-19 22:32:04 -0400

    version changed to v1.7b

--- BH.Injector/BH.Injector.cpp
+++ BH.Injector/BH.Injector.cpp
@@ -110,7 +110,7 @@ int main(int argc, const char* argv[]) {
 	EnumWindows(EnumWindowsProc, (LPARAM)&Windows);
 
 	//Print intro and the beginning of the menu.
-	printf("BH v0.1.7a By McGod\n");
+	printf("BH v0.1.7b By McGod\n");
 	printf("SlashDiablo Branch: Edited By Deadlock, underbent, and Slayergod13\n");
 	printf("Visit http://www.reddit.com/r/slashdiablo for updates!\n");
 
